6e71e3cb87ff8d0ca1e52333c9885d6717b79e98
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-23 22:02:20 +0200

    Creating basic client

---
+++ route_guide_client.py
@@ -0,0 +1,36 @@
+import logging
+
+import grpc
+
+import route_guide_pb2
+import route_guide_pb2_grpc
+
+
+def guide_get_one_feature(stub, point):
+    feature = stub.GetFeature(point)
+    if not feature.location:
+        print("Server returned incomplete feature")
+        return
+
+    if feature.name:
+        print("Feature called %s at %s" % (feature.name, feature.location))
+    else:
+        print("Found no feature at %s" % feature.location)
+
+
+def guide_get_feature(stub):
+    guide_get_one_feature(
+        stub, route_guide_pb2.Point(latitude=409146138, longitude=-746188906))
+    guide_get_one_feature(stub, route_guide_pb2.Point(latitude=0, longitude=0))
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = route_guide_pb2_grpc.RouteGuideStub(channel)
+        print("-------------- GetFeature --------------")
+        guide_get_feature(stub)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
